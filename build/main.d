9fd6be03707473668f92a3418838e577de3fe3ac
megazig <megazig@hotmail.com>
2009-11-23 18:20:32 -0600

    first push of working GetDeviceCert

---
+++ build/main.d
@@ -0,0 +1,172 @@
+main.o: /home/megazig/Wii/GetDeviceCert/source/main.c \
+  /home/megazig/devkitpro/libogc/include/ogcsys.h \
+  /home/megazig/devkitpro/libogc/include/gccore.h \
+  /home/megazig/devkitpro/libogc/include/ogc/dsp.h \
+  /home/megazig/devkitpro/libogc/include/gctypes.h \
+  /home/megazig/devkitpro/libogc/include/ogc/aram.h \
+  /home/megazig/devkitpro/libogc/include/ogc/arqueue.h \
+  /home/megazig/devkitpro/libogc/include/ogc/lwp_queue.h \
+  /home/megazig/devkitpro/libogc/include/ogc/aram.h \
+  /home/megazig/devkitpro/libogc/include/ogc/arqmgr.h \
+  /home/megazig/devkitpro/libogc/include/ogc/audio.h \
+  /home/megazig/devkitpro/libogc/include/ogc/cache.h \
+  /home/megazig/devkitpro/libogc/include/ogc/card.h \
+  /home/megazig/devkitpro/libogc/include/ogc/cast.h \
+  /home/megazig/devkitpro/libogc/include/ogc/color.h \
+  /home/megazig/devkitpro/libogc/include/ogc/consol.h \
+  /home/megazig/devkitpro/libogc/include/ogc/gx_struct.h \
+  /home/megazig/devkitpro/libogc/include/ogc/dvd.h \
+  /home/megazig/devkitpro/libogc/include/ogc/exi.h \
+  /home/megazig/devkitpro/libogc/include/ogc/gu.h \
+  /home/megazig/devkitpro/libogc/include/ogc/gx.h \
+  /home/megazig/devkitpro/libogc/include/ogc/lwp.h \
+  /home/megazig/devkitpro/libogc/include/ogc/gu.h \
+  /home/megazig/devkitpro/libogc/include/ogc/si.h \
+  /home/megazig/devkitpro/libogc/include/ogc/gx_struct.h \
+  /home/megazig/devkitpro/libogc/include/ogc/irq.h \
+  /home/megazig/devkitpro/libogc/include/ogc/context.h \
+  /home/megazig/devkitpro/libogc/include/ogc/lwp.h \
+  /home/megazig/devkitpro/libogc/include/ogc/mutex.h \
+  /home/megazig/devkitpro/libogc/include/ogc/message.h \
+  /home/megazig/devkitpro/libogc/include/ogc/semaphore.h \
+  /home/megazig/devkitpro/libogc/include/ogc/pad.h \
+  /home/megazig/devkitpro/libogc/include/ogc/tpl.h \
+  /home/megazig/devkitpro/libogc/include/ogc/gx.h \
+  /home/megazig/devkitpro/libogc/include/ogc/system.h \
+  /home/megazig/devkitpro/libogc/include/gcutil.h \
+  /home/megazig/devkitpro/libogc/include/ogc/video.h \
+  /home/megazig/devkitpro/libogc/include/ogc/video_types.h \
+  /home/megazig/devkitpro/libogc/include/ogc/usbgecko.h \
+  /home/megazig/devkitpro/libogc/include/ogc/video_types.h \
+  /home/megazig/devkitpro/libogc/include/ogc/texconv.h \
+  /home/megazig/devkitpro/libogc/include/ogc/ipc.h \
+  /home/megazig/devkitpro/libogc/include/ogc/es.h \
+  /home/megazig/devkitpro/libogc/include/ogc/stm.h \
+  /home/megazig/devkitpro/libogc/include/ogc/ios.h \
+  /home/megazig/devkitpro/libogc/include/ogc/usb.h \
+  /home/megazig/devkitpro/libogc/include/ogc/isfs.h \
+  /home/megazig/devkitpro/libogc/include/ogc/conf.h \
+  /home/megazig/devkitpro/libogc/include/ogc/usbstorage.h \
+  /home/megazig/devkitpro/libogc/include/ogc/mutex.h \
+  /home/megazig/devkitpro/libogc/include/ogc/disc_io.h \
+  /home/megazig/devkitpro/libogc/include/ogc/wiilaunch.h \
+  /home/megazig/devkitpro/libogc/include/wiiuse/wpad.h \
+  /home/megazig/devkitpro/libogc/include/wiiuse/wiiuse.h \
+  /home/megazig/devkitpro/libogc/include/bte/bte.h \
+  /home/megazig/devkitpro/libogc/include/bte/bd_addr.h \
+  /home/megazig/devkitpro/libogc/include/elm.h \
+  /home/megazig/devkitpro/libogc/include/usync.h
+
+/home/megazig/devkitpro/libogc/include/ogcsys.h:
+
+/home/megazig/devkitpro/libogc/include/gccore.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/dsp.h:
+
+/home/megazig/devkitpro/libogc/include/gctypes.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/aram.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/arqueue.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/lwp_queue.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/aram.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/arqmgr.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/audio.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/cache.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/card.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/cast.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/color.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/consol.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/gx_struct.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/dvd.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/exi.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/gu.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/gx.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/lwp.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/gu.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/si.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/gx_struct.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/irq.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/context.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/lwp.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/mutex.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/message.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/semaphore.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/pad.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/tpl.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/gx.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/system.h:
+
+/home/megazig/devkitpro/libogc/include/gcutil.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/video.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/video_types.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/usbgecko.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/video_types.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/texconv.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/ipc.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/es.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/stm.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/ios.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/usb.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/isfs.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/conf.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/usbstorage.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/mutex.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/disc_io.h:
+
+/home/megazig/devkitpro/libogc/include/ogc/wiilaunch.h:
+
+/home/megazig/devkitpro/libogc/include/wiiuse/wpad.h:
+
+/home/megazig/devkitpro/libogc/include/wiiuse/wiiuse.h:
+
+/home/megazig/devkitpro/libogc/include/bte/bte.h:
+
+/home/megazig/devkitpro/libogc/include/bte/bd_addr.h:
+
+/home/megazig/devkitpro/libogc/include/elm.h:
+
+/home/megazig/devkitpro/libogc/include/usync.h:
---
+++ source/main.c
@@ -0,0 +1,347 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <ogcsys.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include <sys/dir.h>
+#include <errno.h>
+
+#include <elm.h>
+#include <usync.h>
+
+#define		DEBUG			0
+
+#define		EFATINIT		-1
+#define		ENETINIT		-2
+
+#define		EELMMOUNT		-3
+
+// Colors
+#define		BLACK_FG		30
+#define		BLACK_BG		40
+#define		RED_FG			31
+#define		RED_BG			41
+#define		GREEN_FG		32
+#define		GREEN_BG		42
+#define		YELLOW_FG		33
+#define		YELLOW_BG		43
+#define		BLUE_FG			34
+#define		BLUE_BG			44
+#define		MAGENTA_FG		35
+#define		MAGENTA_BG		45
+#define		CYAN_FG			36
+#define		CYAN_BG			46
+#define		WHITE_FG		37
+#define		WHILTE_BG		47
+
+// Text Styles
+#define		RESET_TEXT		0
+#define		BRIGHT_TEXT		1
+#define		DIM_TEXT		2
+#define		UNDERSCORE_TEXT	3
+#define		BLINKING_TEXT	4
+#define		REVERSE_TEXT	5
+#define		HIDDEN_TEXT		6
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+s32 ExitRequested = 0;
+s8 HWButton = -1;
+
+//--------------------------------------------------------------------------------
+void WiiResetPressed() {
+//--------------------------------------------------------------------------------
+//
+//	Params:		None
+//	Returns:	None
+//
+	HWButton = SYS_RETURNTOMENU;
+}
+
+//--------------------------------------------------------------------------------
+void WiiPowerPressed() {
+//--------------------------------------------------------------------------------
+//
+//	Params:		None
+//	Returns:	None
+//
+	HWButton = SYS_POWEROFF_STANDBY;
+}
+
+//--------------------------------------------------------------------------------
+void WiimotePowerPressed( int channel ) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		channel		Channel of wiimote whose power button was pressed
+//	Returns:	None
+//
+	HWButton = SYS_POWEROFF_STANDBY;
+}
+
+//--------------------------------------------------------------------------------
+void SetTextInfo( int color_fg , int color_bg , int attribute ) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		color_fg			Foreground Color For Text
+//				color_bg			Background Color For Text
+//				attribute			Text Style
+//	Returns:	None
+//
+
+	printf( "\x1b[%dm" , attribute );
+	printf( "\x1b[%dm" , color_fg );
+	printf( "\x1b[%dm" , color_bg );
+}
+
+//--------------------------------------------------------------------------------
+void ClearText( ) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		None
+//	Returns:	None
+//
+
+	printf( "\x1b[2J" );
+}
+
+//--------------------------------------------------------------------------------
+void PrintPositioned( int y , int x , const char * text ) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		y			Y Position for Text
+//				x			X Position for Text
+//				string		Strig to Print
+//	Returns:	None
+//
+
+	printf("\x1b[%d;%dH", y , x );
+	printf("%s" , text );
+}
+
+//--------------------------------------------------------------------------------
+void ExitToLoader( int return_val ) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		return_val	Value to Pass to Exit
+//	Return:		None		exits
+//
+
+	printf("\x1b[25;5HThanks for using DeviceCertDumper\n");
+	exit( return_val );
+}
+
+//--------------------------------------------------------------------------------
+void WaitForButtonPress() {
+//--------------------------------------------------------------------------------
+//
+//	Params:		None
+//	Return:		None
+//
+
+	while(1) {
+
+		// Call WPAD_ScanPads each loop, this reads the latest controller states
+		WPAD_ScanPads();
+
+		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
+		// this is a "one shot" state which will not fire again until the button has been released
+		u32 pressed = WPAD_ButtonsDown(0);
+
+		// We return to the launcher application when home is pressed
+		if ( pressed & WPAD_BUTTON_HOME ) ExitToLoader(0);
+		// We break the waiting loop with any other buttons
+		if ( pressed & WPAD_BUTTON_A ) break;
+		if ( pressed & WPAD_BUTTON_B ) break;
+		if ( pressed & WPAD_BUTTON_1 ) break;
+		if ( pressed & WPAD_BUTTON_2 ) break;
+		if ( pressed & WPAD_BUTTON_PLUS ) break;
+		if ( pressed & WPAD_BUTTON_MINUS ) break;
+		if ( pressed & WPAD_BUTTON_UP ) break;
+		if ( pressed & WPAD_BUTTON_DOWN ) break;
+		if ( pressed & WPAD_BUTTON_LEFT ) break;
+		if ( pressed & WPAD_BUTTON_RIGHT ) break;
+
+		// Add check for Reset or Power button
+		if ( HWButton != -1 )
+			SYS_ResetSystem( HWButton , 0 , 0 );
+
+		// Wait for the next frame
+		VIDEO_WaitVSync();
+	}
+}
+
+//--------------------------------------------------------------------------------
+void ShowProgramInfo() {
+//--------------------------------------------------------------------------------
+//
+//	Params:		None
+//	Returns:	None
+//
+
+	printf("\x1b[2;0H");
+	printf("DeviceCertDumper\n");
+	printf("coded by\n");
+	SetTextInfo( BLINKING_TEXT , GREEN_FG , BLACK_BG );
+	printf("megazig\n");
+	SetTextInfo( RESET_TEXT , WHITE_FG , BLACK_BG );
+}
+
+//--------------------------------------------------------------------------------
+char ascii(char s) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		char s
+//	Returns:	None
+//
+  if(s < 0x20) return '.';
+  if(s > 0x7E) return '.';
+  return s;
+}
+
+//--------------------------------------------------------------------------------
+void hexdump(FILE *fp, void *d, int len) {
+//--------------------------------------------------------------------------------
+//
+//	Params:		FILE *fp , void *d , int len
+//	Returns:	None
+//
+	u8 *data;
+	int i, off;
+	data = (u8*)d;
+	for (off=0; off<len; off += 16) {
+		fprintf(fp, "%08x  ",off);
+		for(i=0; i<16; i++)
+			if((i+off)>=len) fprintf(fp, "  ");
+			else fprintf(fp, "%02x ",data[off+i]);
+
+		fprintf(fp, " ");
+		for(i=0; i<16; i++)
+			if((i+off)>=len) fprintf(fp," ");
+			else fprintf(fp,"%c",ascii(data[off+i]));
+		fprintf(fp,"\n");
+	}
+}
+
+//-----------------------------------------------------------------------------
+int SetupELM()
+//-----------------------------------------------------------------------------
+{
+	uSyncInit();
+	int ret = ELM_Mount();
+	if(ret & 1) {
+		printf("Unable to mount SD  %d\n", ELM_GetMountError(ELM_SD));
+		return -1;
+	}
+	if(ret & 2){
+		printf("Unable to mount USB %d\n", ELM_GetMountError(ELM_USB));
+	}
+	if(ret & 4) {
+		printf("Unable to mount GCA %d\n", ELM_GetMountError(ELM_GCA));
+	}
+	if(ret & 8) {
+		printf("Unable to mount GCB %d\n", ELM_GetMountError(ELM_GCB));
+	}
+	return 0;
+}
+
+//--------------------------------------------------------------------------------
+void Init() {
+//--------------------------------------------------------------------------------
+//
+//	Params:		None
+//	Returns:	None
+//
+
+	// Initialise the video system
+	VIDEO_Init();
+	
+	// This function initialises the attached controllers
+	WPAD_Init();
+	
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+	
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+	
+	// Make the display visible
+	VIDEO_SetBlack(FALSE);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+	SYS_SetResetCallback( WiiResetPressed );
+	SYS_SetPowerCallback( WiiPowerPressed );
+	WPAD_SetPowerButtonCallback( WiimotePowerPressed );
+
+	ClearText();
+	printf("\x1b[2;0H");
+	ShowProgramInfo();
+
+	PrintPositioned( 15 , 0 , "Initializing FAT File System.........." );
+	uSyncInit();
+	int ret = ELM_Mount();
+	if ( ret & 1 ) {
+		PrintPositioned( 15 , 45 , "ELM_Mount failed :( \n" );
+		printf("ELM_Mount returned: %d\n", ret);
+		WaitForButtonPress();
+		ExitToLoader( EELMMOUNT );
+	}
+	PrintPositioned( 15 , 45 , "COMPLETE\n" );
+}
+
+//--------------------------------------------------------------------------------
+int main(int argc, char **argv)
+//--------------------------------------------------------------------------------
+{
+	Init();
+
+	int i;
+	FILE *fp = NULL;
+
+	if(chdir("sd:/sys") != 0)
+		mkdir("sd:/sys", 0);
+	fp = fopen("sd:/sys/device.cert", "w");
+	if ( fp == NULL ) {
+		printf( "Error opening device.cert" );
+		int err = ELM_GetError();
+		printf( "Error code: %d\n" , err );
+		ELM_Unmount();
+		WaitForButtonPress();
+		ExitToLoader(1);
+	}
+
+	if (fp) {
+		u8 * devcert = memalign( 32 , 0x200 );
+		memset(devcert, 42, 0x200);
+		i = ES_GetDeviceCert(devcert);
+		if (i) printf("ES_GetDeviceCert returned %d\n", i);
+		else {
+			fwrite(devcert, 0x180, 1, fp);
+		}
+		fclose(fp);
+		ELM_Unmount();
+		free(devcert);
+	}
+	sleep(10);
+	return 0;
+}
+
